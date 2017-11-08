61802278c69efdec48583b5b1fc6aab07fbe4217
Enes Witwit <witwit@stud.uni-heidelberg.de>
2017-11-08 09:58:25 +0000

    Deleted database

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Einbürgerungstest 2017'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://einbuergerungstest-2017.de'),
 
     /*
     |--------------------------------------------------------------------------
